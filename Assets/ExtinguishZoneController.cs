ee34cf74e2705e6d60842c9914e3ba8a9ae1cd65
Mutian(Mu) Chen <muvetian@gmail.com>
2026-01-17 21:15:01 +0800

    [Env] Build transition level from 6 to 7

---
+++ Assets/ExtinguishZoneController.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExtinguishZoneController : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            PlayerController player = collision.gameObject.GetComponent<PlayerController>();
+            player.ifStateTransitionPerm = false;
+            player.StartExtinguished();
+        }
+    }
+}
